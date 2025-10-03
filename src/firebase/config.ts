4954f8987292fc9b161d53b54ad9c78d70870f8c
Mine Free Gamez <minefreegamez@gmail.com>
2025-10-03 06:08:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2143086067-fddbb",
+  "appId": "1:927002962785:web:8f65b44a35fd1b5a37b320",
+  "apiKey": "AIzaSyAzPyKZEw4MjvNw5q2VTkSzUi8zgbVRv7c",
+  "authDomain": "studio-2143086067-fddbb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "927002962785"
+};
