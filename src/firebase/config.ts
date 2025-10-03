9d9e9836a5ceeb5d91aff9f65b39861ec8c4f425
Mine Free Gamez <minefreegamez@gmail.com>
2025-10-03 06:32:55 +0000

    Try fixing this error: `Console Error: FirebaseError: Firebase Storage:

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
   "projectId": "studio-2143086067-fddbb",
   "appId": "1:927002962785:web:8f65b44a35fd1b5a37b320",
+  "storageBucket": "studio-2143086067-fddbb.appspot.com",
   "apiKey": "AIzaSyAzPyKZEw4MjvNw5q2VTkSzUi8zgbVRv7c",
-  "authDomain": "studio-2143086067-fddbb.firebaseapp.com",
+  "authDomain": "studio-2143_6067-fddbb.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "927002962785"
 };
